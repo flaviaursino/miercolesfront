f4a27130c965714a8477fc0b8348e0e741306446
Flavia Ursino <flavia.ursino@gmail.com>
2025-09-10 18:34:13 -0300

    archivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import './App.css';
+import Header from './compontes/layout/Header';
+import Nav from './compontes/layout/Nav';
+import Footer from './compontes/layout/Footer';
+
+import ContactoPage from './pages/ContactoPage';
+import HomePage from './pages/HomePage';
+import NosotrosPage from './pages/NosotrosPage';
+import NovedadesPage from './pages/NovedadesPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      <Header />
+      <BrowserRouter>
+     
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="nosotros" element={<NosotrosPage />} />
+          <Route path="novedades" element={<NovedadesPage />} />
+          <Route path="contacto" element={<ContactoPage />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
+
     </div>
   );
 }
